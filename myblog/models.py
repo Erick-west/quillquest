693d9315c2da7c3e3dff08495ca7f85f9774844d
Fidel <fidelmasitsa03@gmail.com>
2023-12-25 04:55:56 +0300

    fidel's work

--- myblog/models.py
+++ myblog/models.py
@@ -1,11 +1,10 @@
+# models.py
 from django.db import models
-from django.contrib.auth.models import User
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()  # No need to limit content for display
+    date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.title} | {self.author}"
-
+        return self.title
--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Blog
+from django.utils import timezone
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-def home(request):
-    return render(request, 'home.html', {})
+def blog_list(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog_list.html', {'blogs': blogs})
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    return render(request, 'blog_detail.html', {'blog': blog})
+
+def blog_new(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')  # No content limit
+        date_posted = timezone.now()
+
+        Blog.objects.create(title=title, content=content, date_posted=date_posted)
+        return redirect('blog:blog_list')
+
+    return render(request, 'blog_edit.html', {'edit_mode': False})
+
+def blog_edit(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+
+    if request.method == "POST":
+        blog.title = request.POST.get('title')
+        blog.content = request.POST.get('content')  # No content limit
+        blog.save()
+        return redirect('blog:blog_list')
+
+    return render(request, 'blog_edit.html', {'edit_mode': True, 'blog': blog})
+
+def blog_delete(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    blog.delete()
+    return redirect('blog:blog_dashboard')
+
+@login_required(login_url='blog:login')
+def blog_dashboard(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog_dashboard.html', {'blogs': blogs})
+
+from django.contrib.auth.models import User
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Check if the username already exists
+        if User.objects.filter(username=username).exists():
+            # Handle the case where the username already exists (you can redirect to an error page or display a message)
+            return render(request, 'signup.html', {'error_message': 'Username already exists'})
+
+        # Create a new user
+        user = User.objects.create_user(username=username, password=password)
+
+        # Authenticate and log in the user
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('blog:blog_dashboard')
+
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('blog:blog_dashboard')
+    return render(request, 'login.html')
+
+def signout(request):
+    logout(request)
+    return redirect('blog:blog_list')
